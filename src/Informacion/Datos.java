37e5f289ecd801e5fba3630b67d40f7fd6c11f7f
panch <panch@Mendxza>
2022-04-05 13:34:04 -0500

    PRACTICA 12

---
+++ src/Informacion/Datos.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Informacion;
+
+/**
+ *
+ * @author panch
+ */
+public class Datos {
+    
+    String nombre;
+    int edad;
+    String Correo;
+
+    public Datos() {
+    }
+
+    public Datos(String nombre, int edad, String Correo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.Correo = Correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+    
+    
+    
+}
---
+++ src/Procesos/Almacen.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Procesos;
+
+import Informacion.Datos;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author panch
+ */
+public class Almacen {
+    public void grabar(String cadena){
+        try{
+            FileWriter  archivo = new FileWriter("Datos.txt", true);
+            BufferedWriter almacena = new BufferedWriter(archivo);
+            
+            almacena.write(cadena + "\n");
+            
+            almacena.close();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+    
+    public ArrayList<String> leer(){
+        ArrayList<String> informacion = new ArrayList<String>();
+        
+        try{
+            FileReader archivo = new FileReader("Datos.txt");
+            BufferedReader datos = new BufferedReader(archivo);
+            String cadena;
+            
+            while((cadena = datos.readLine())!= null){
+                informacion.add(cadena);
+            }
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        
+        return informacion;
+    }
+
+    public void borrar(String cadena) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
